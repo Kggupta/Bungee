a2b03b527f20576e1cc34d531b79a41826b0bc49
Kggupta <33162944+Kggupta@users.noreply.github.com>
2022-05-28 11:06:28 -0400

    Bottom bar views

---
+++ src/Views/Data.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { DarkTheme, Button, Provider as PaperProvider } from 'react-native-paper';
+
+const Data = () => {
+    return (
+      <View>
+        <Text>Todo</Text>
+      </View>
+    );
+  };
+
+export default Data;
---
+++ src/Views/Settings.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { DarkTheme, Button, Provider as PaperProvider } from 'react-native-paper';
+
+const Settings = () => {
+    return (
+      <View>
+        <Text>Todo</Text>
+      </View>
+    );
+  };
+
+export default Settings;
