02814550853acd7ba98527f1322e3e678172c522
Kggupta <33162944+Kggupta@users.noreply.github.com>
2022-05-31 17:37:15 -0400

    persistant storage

---
+++ src/Utilities/UseAsyncStorage.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {SHEET_ID} from '@env'
+const SHEET_ID_KEY = "SHEET_ID";
+
+/**
+ * Save the sheet id in async storage
+ * @param id - Sheet ID
+ */
+async function saveSheetId(id : string) {
+    await AsyncStorage.setItem(SHEET_ID_KEY, id);
+}
+
+/**
+ * Get the sheet id from async storage
+ * @returns The sheet ID
+ */
+async function getSheetId() : Promise<string> {
+    return await AsyncStorage.getItem(SHEET_ID_KEY) || '';
+}
+
+export {saveSheetId, getSheetId};
