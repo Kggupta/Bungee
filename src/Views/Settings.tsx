b538d66cd82fc6f49a59601ef0aa46cd702942a8
Kggupta <33162944+Kggupta@users.noreply.github.com>
2022-06-12 08:35:38 -0400

    Pick term component

--- src/Views/Settings.tsx
+++ src/Views/Settings.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { DarkTheme, Button, Provider as PaperProvider } from 'react-native-paper';
+import PickTerm from './Components/PickTerm';
 import SheetComponent from './Components/SheetComponent';
 
 
@@ -8,6 +9,7 @@ const Settings = () => {
     return (
       <View>
         <SheetComponent />
+        <PickTerm />
       </View>
     );
 };
