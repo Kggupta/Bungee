12188144792620b487d41f31b08c8cc29d68e14a
Kggupta <33162944+Kggupta@users.noreply.github.com>
2022-06-18 14:35:53 -0400

    Dark theme for navigation container

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import Tabs from './src/Navigation/Nav';
 
 export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer>
+      <NavigationContainer theme={DarkTheme}>
         <Tabs />
       </NavigationContainer>
     </PaperProvider>
